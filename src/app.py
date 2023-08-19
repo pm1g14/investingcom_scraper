1dd2fda014cd2c29c7a1c219aaf138ea02a13a74
unknown <panamavr@hotmail.com>
2023-08-19 13:25:27 +0100

    making selenium scraper waiting for live event to avoid delay in finding the actual value

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
@@ -6,10 +7,14 @@ from model import RowParameters
 from utils import NumUtils as util
 from utils import JsonUtils, DateUtils
 from publisher import ZmqPublisher
-from scraper import InvestingComLightWeightScraper
+from scraper import InvestingComLightWeightScraper, InvestingComCsScaper, InvestingComScraper
 from bs4 import BeautifulSoup as BSoup
+from selenium.webdriver.support.ui import WebDriverWait
+from optional import Optional
+from selenium.webdriver.support import expected_conditions as EC
 
 import logging, time
+import cfscrape
 
 logging.basicConfig(filename="applogs", format='%(asctime)s%(message)s', filemode='w')
 
@@ -32,8 +37,7 @@ def getDriver():
     driver = webdriver.Chrome(options = chrome_options)
     return driver
 
-
-if __name__ == '__main__':
+def invoke_scraper():
     publisher =  ZmqPublisher()
     investingcom_scraper = InvestingComLightWeightScraper()
 
@@ -56,3 +60,74 @@ if __name__ == '__main__':
         print(f'Time to load and scrape: {final} seconds')
         logging.debug(f'Time to load and scrape: {final} seconds')
         driver.close()
+
+def invoke_cs_scraper():
+    publisher =  ZmqPublisher()
+
+    while True:
+        start_timestamp = time.time()
+
+        scraper = cfscrape.create_scraper()
+        response = scraper.get(f"https://www.investing.com/economic-calendar/")
+        investing_com_cs_scraper = InvestingComCsScaper()
+        rows = investing_com_cs_scraper.scrape(driver = response)
+        #send with zeromq
+        message:dict = JsonUtils().convertListToJson(elements= rows)
+        logging.info(message)
+        print(message)
+
+        if rows:
+            publisher.publish(message=message)
+
+        end_timestamp = time.time()
+        final = end_timestamp - start_timestamp
+        print(f'Time to load and scrape: {final} seconds')
+        logging.debug(f'Time to load and scrape: {final} seconds')
+
+def getParam(parameter:str, args) -> str | None:
+    if args[0] == parameter:
+        value = args[1]
+    elif args[2] == parameter:
+        value = args[3]
+    else:
+        value = None
+    return value
+
+def process_input():
+    try:
+        args = sys.argv[1:]
+        maybe_event_to_wait_for = getParam("--liveEventToWaitFor", args)
+        maybe_currency = getParam("--currency", args)
+        return maybe_event_to_wait_for, maybe_currency
+    except Exception:
+        return None, None
+
+def invoke_selenium_scraper():
+    publisher =  ZmqPublisher()
+    scraper = InvestingComScraper()
+    driver = getDriver()
+    maybe_live_event_to_wait_for, currency = process_input()
+
+    while True:
+        logging.debug(f'Starting parsing at: {time.time()}')
+        start_timestamp = time.time()
+
+        driver.get(f"https://www.investing.com/economic-calendar/")
+        rows = scraper.scrape(driver, maybe_live_event_to_wait_for, currency)
+        #send with zeromq
+        message:dict = JsonUtils().convertListToJson(elements= rows)
+        logging.info(message)
+        print(message)
+
+        if rows:
+            publisher.publish(message=message)
+
+        end_timestamp = time.time()
+        final = end_timestamp - start_timestamp
+        print(f'Time to load and scrape: {final} seconds')
+        logging.debug(f'Time to load and scrape: {final} seconds')
+
+
+if __name__ == '__main__':
+    invoke_selenium_scraper()
+    
--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ from datetime import date
 import calendar
 import time
 import json
+import datetime
 
 class NumUtils:
 
@@ -29,6 +30,36 @@ class JsonUtils:
 
 
 class DateUtils:
+
+    def convertDateAndTimeStrToDatetime(time:str, date: date):
+        format = r"%Y-%m-%d %H:%M:%S"
+        try:
+            datetimeStr = f"{date} {time}"
+            datetimeObj = datetime.datetime.strptime(datetimeStr, format)
+            return datetimeObj
+        except Exception:
+            print('Cannot convert given time and date to a datetime object')
+            return None
+
+
+
+    def convertStrTimeToTime(time:str):
+        format = r"%H:%M:%S"
+        try:
+            timeObj = datetime.datetime.strptime(time, format).time()
+            return timeObj
+        except Exception:
+            print('Cannot convert given time string to time object')
+            return None
+
+    def convertStrToDate(date:str) -> datetime.datetime:
+        format = r"%b %d %Y"
+        try:
+            datetimeObj = datetime.datetime.strptime(date, format)
+            return datetimeObj
+        except Exception as e:
+            print('Cannot convert given string to the desired datetime object')
+            return ''
 
     def getInvestingComDateFormat(date:date) -> str:
         format = r"%b %d %Y"
